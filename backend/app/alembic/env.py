dfe2314af68eaded2d063c8a04ad83e32b88f9a5
Vickyyy <johnsalen13@gmail.com>
2026-09-26 20:15:50 +0800

    fix: remove unused alembic import

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -1,11 +1,8 @@
-import os
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-# this is the Alembic Config object, which provides
-# access to the Python Alembic environment.
 config = context.config
 
 assert config.config_file_name is not None
