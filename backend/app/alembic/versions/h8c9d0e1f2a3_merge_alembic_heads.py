3b409b4daa37825d461c5d35bc6b7ee5d15a9807
Vickyyy <johnsalen13@gmail.com>
2026-09-26 21:46:15 +0800

    fix: merge divergent Alembic heads

---
+++ backend/app/alembic/versions/h8c9d0e1f2a3_merge_alembic_heads.py
@@ -0,0 +1,18 @@
+"""merge the academic-year and import-tracking migration branches
+
+Revision ID: h8c9d0e1f2a3
+Revises: da02e2b15aa8, g7b8c9d0e1f2
+"""
+
+revision = "h8c9d0e1f2a3"
+down_revision = ("da02e2b15aa8", "g7b8c9d0e1f2")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
